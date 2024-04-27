f41cb6a67f6744b51018a3e41f9ffcfc8ad7d76c
Armin Lachini <armin@lachini.net>
2024-04-27 23:15:45 +0200

    add consistent db for dev/testing

---
+++ books/src/main/resources/data.sql
@@ -0,0 +1,38 @@
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Brave New World', '9780060850524', NULL, '2006-08-17', 288, 'English', 'DYSTOPIAN');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Schöne Neue Welt', '9783596905737', NULL, '2014-03-06', 368, 'German', 'DYSTOPIAN');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Rick Astley: The Biography', '9798880228621', NULL, '2024-02-20', 40, 'English', 'BIOGRAPHY');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Hitchhiker''s Guide to the Galaxy', '9781529034523', NULL, '2020-03-05', 224, 'English', 'SCIENCE_FICTION');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Magic Fermentation', '9783706626866', NULL, '2021-06-10', 304, 'German', 'COOKBOOKS');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Instabil - Die Vergangenheit ist noch nicht geschehen', '9781979346610', NULL, '2017-10-31', 430, 'German', 'THRILLER');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Speed of Sound - Das Echo der Erinnerung', '9782919805402', NULL, '2019-01-08', 489, 'German', 'THRILLER');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Blackout - Morgen ist es zu spät', '9783442380299', NULL, '2013-06-17', 832, 'German', 'THRILLER');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Journey to Jo''burg', '9780007263509', NULL, '2008-03-03', 112, 'French', 'CHILDREN');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('The Great Dune Trilogy', '9781473224469', NULL, '2018-08-23', 912, 'English', 'SCIENCE_FICTION');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Necronomicon', '9780575081567', NULL, '2008-03-27', 896, 'English', 'HORROR');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Overcoming Gravity', '9780990873853', NULL, '2016-11-18', 590, 'English', 'HEALTH');
+
+INSERT INTO book_entity (title, isbn, publisher, date_published, pages, language, genre)
+VALUES ('Endurance: Shackleton''s Incredible Voyage', '9780465062881', NULL, '2015-04-01', 416, 'English', 'ADVENTURE');
