a010bc6ad3afd442c4b3d86bc3f4c78fa1913f02
Armin Lachini <armin@lachini.net>
2024-04-29 11:47:51 +0200

    add environment variable and tests

--- frontend/src/app/services/book.service.spec.ts
+++ frontend/src/app/services/book.service.spec.ts
@@ -1,16 +1,96 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { BookService } from './book.service';
+import { environment } from '../../../environment';
 
 describe('BookService', () => {
   let service: BookService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BookService],
+    });
     service = TestBed.inject(BookService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  // afterEach(() => {
+  //   httpMock.verify();
+  // });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get list of books', () => {
+    const mockBooks = [{ title: 'Book 1' }, { title: 'Book 2' }];
+
+    service.getBooks().subscribe((books) => {
+      expect(books.length).toBe(2);
+      expect(books).toEqual(mockBooks);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/books`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockBooks);
+  });
+
+  it('should get compensation by ISBN', () => {
+    const mockCompensation = { amount: 10, currency: 'EUR', details: {} };
+
+    let isbn: string = '0000000000000';
+
+    service.getCompensationByIsbn(isbn).subscribe((compensation) => {
+      expect(compensation).toEqual(mockCompensation);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiBaseUrl}/books/${isbn}/compensation`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockCompensation);
+  });
+
+  it('should create a new book', () => {
+    const newBook = { title: 'New Book', author: 'Author' };
+
+    service.createBook(newBook).subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/books`);
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+  });
+
+  it('should get list of genres', () => {
+    const mockGenres = ['Fiction', 'Non-Fiction', 'Mystery'];
+
+    service.getGenres().subscribe((genres) => {
+      expect(genres.length).toBe(3);
+      expect(genres).toEqual(mockGenres);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/books/genres`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockGenres);
+  });
+
+  it('should get list of languages', () => {
+    const mockLanguages = ['English', 'French', 'Spanish'];
+
+    service.getLanguages().subscribe((languages) => {
+      expect(languages.length).toBe(3);
+      expect(languages).toEqual(mockLanguages);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/books/languages`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockLanguages);
   });
 });
--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -3,34 +3,33 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book.model';
 import { BookCompensation } from '../models/compensation.model';
+import { environment } from '../../../environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private apiBaseUrl = 'http://127.0.0.1:8080/books';
-
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiBaseUrl);
+    return this.http.get<Book[]>(`${environment.apiBaseUrl}/books`);
   }
 
   getCompensationByIsbn(isbn: string): Observable<BookCompensation> {
     return this.http.get<BookCompensation>(
-      `${this.apiBaseUrl}/${isbn}/compensation`
+      `${environment.apiBaseUrl}/books/${isbn}/compensation`
     );
   }
 
   createBook(book: any): Observable<any> {
-    return this.http.post<any>(this.apiBaseUrl, book);
+    return this.http.post<any>(`${environment.apiBaseUrl}/books`, book);
   }
 
   getGenres(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiBaseUrl}/genres`);
+    return this.http.get<string[]>(`${environment.apiBaseUrl}/books/genres`);
   }
 
   getLanguages(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiBaseUrl}/languages`);
+    return this.http.get<string[]>(`${environment.apiBaseUrl}/books/languages`);
   }
 }
